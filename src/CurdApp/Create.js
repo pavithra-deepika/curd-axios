a38089f24cfc336ceb09ee5433641a204af06dc9
sangi19 <sangiammu1020@gmail.com>
2023-08-12 23:10:11 +0530

    new user add to the json server data

--- src/CurdApp/Create.js
+++ src/CurdApp/Create.js
@@ -1,21 +1,39 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function Create() {
+    const [inputData, setInputData] = useState({
+        name:'',
+        email: ''
+    })
+    const navigate = useNavigate()
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:3000/users', inputData) 
+        .then(res => {
+            alert("Data post sucessfully!")
+            navigate('/')
+        })
+    }
   return (
     <div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
         <div className='input'>
-            <fotm>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor='name'>Name:</label>
-                    <input type='text' className='form-control' name='name' />
+                    <input type='text' className='form-control' name='name'
+                    onChange={e => setInputData({...inputData, name:e.target.value})} />
                 </div>
                 <div>
                     <label htmlFor='email'>Email:</label>
-                    <input type='text' className='form-control' name='email' />
+                    <input type='text' className='form-control' name='email' 
+                    onChange={e => setInputData({...inputData, email:e.target.value})} />
+
                     <br />
                     <button className='btn btn-info'>Submit</button>
                 </div>
-            </fotm>
+            </form>
         </div>
         
     </div>
