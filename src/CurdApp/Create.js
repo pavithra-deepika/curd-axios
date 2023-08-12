4afbc78fefe6e013188672caf9c449ebc8bfd86e
sangi19 <sangiammu1020@gmail.com>
2023-08-12 16:32:46 +0530

    ceate input field

--- src/CurdApp/Create.js
+++ src/CurdApp/Create.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Create() {
   return (
-    <div>Create</div>
+    <div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
+        <div className='input'>
+            <fotm>
+                <div>
+                    <label htmlFor='name'>Name:</label>
+                    <input type='text' className='form-control' name='name' />
+                </div>
+                <div>
+                    <label htmlFor='email'>Email:</label>
+                    <input type='text' className='form-control' name='email' />
+                    <br />
+                    <button className='btn btn-info'>Submit</button>
+                </div>
+            </fotm>
+        </div>
+        
+    </div>
   )
 }
 
